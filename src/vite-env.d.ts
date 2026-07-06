8cc70c699f5735df4dc35b2d0e1a62a1ded4104f
Simon van Lierde <simonvanlierde@gmail.com>
2026-07-06 22:59:11 +0200

    chore(build): expose app version via __APP_VERSION__

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
+
+/** App version, injected from package.json at build time by Vite. */
+declare const __APP_VERSION__: string;
